1e5deb180b5e886c4f78e8432a4236b7d89e7a1c
Nakse Veyla <72164371+nauxxe@users.noreply.github.com>
2024-10-11 19:12:50 +0700

    Create index

---
+++ index.py
@@ -0,0 +1,7 @@
+class Naksena:
+    def __init__(self, name):
+        self.name = name
+    
+    def introduce(self):
+        print(f"Hello, I'm {self.name}!")
+        print(f"I am still learning about program code Python.")
